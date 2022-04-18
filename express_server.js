2c4432ced8fffa82a3fcf8768e32e03c4f0addb1
Brandon Felty <brcfelty@gmail.com>
2022-04-18 17:10:35 -0600

    added somes routes to test how variables are referenced with each app.get reponse

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const req = require('express/lib/request');
 const app = express();
 const PORT = 8080;
 
@@ -17,7 +18,16 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b><html>\n");
-})
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+app.get('/fetch', (req, res) => {
+  res.send(`a = ${a}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
