c8324f936ae00ed1488a5364f58ff6c643f1a63e
Brandon Felty <brcfelty@gmail.com>
2022-04-21 16:38:04 -0600

    added a new file to test the helper functions with mocha and chai

---
+++ test/helpersTest.js
@@ -0,0 +1,28 @@
+const { assert } = require('chai');
+
+const { searchForEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', () => {
+    const user = searchForEmail(testUsers, "user@example.com");
+    const expectedUserID = "userRandomID";
+    assert.deepEqual(user, testUsers[expectedUserID]);
+  });
+  it('should return false when the email is not in our user database', () => {
+    const user = searchForEmail(testUsers, "notAUser@example.com");
+    assert.deepEqual(user, false);
+  });
+});
